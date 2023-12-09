83e6ab901ecd446522c1a405253b6a8177adbcb6
Maurits Beudeker <mauritsbeu@gmail.com>
2023-12-09 13:37:43 +0100

    started day 1-2

--- day1/src/lib.rs
+++ day1/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn add_numbers(string: String) -> i32 {
     let mut a: i32 = 0; // initialize var a
     let mut  ab: i32 = 0;
@@ -24,13 +26,51 @@ pub fn add_numbers(string: String) -> i32 {
     return sum
 }
 
+pub fn add_numbers_2(string: String) -> i32 {
+    let numbers_dict = HashMap::from([
+        ("zero", 0),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+    let mut a: i32 = 0; // initialize var a
+    let mut  ab: i32 = 0;
+    let mut sum:i32 = 0;
+    let mut trigger: bool = false;
+    
+    for c  in string.chars() {
+        if let Some(digit) =  c.to_digit(10) {
+            if !trigger {
+                a = digit as i32;  // if char is the 1st number in the string, save it to 'a'
+                trigger = true;             // 1st character has been found, don't save a on the next char
+            }
+            let b: i32 = digit as i32;
+            ab = a *10 + b;
+        }
+        //println!("{c}")
+        if (c as i32) == 10 {
+            // println!("{ab}");
+            sum = sum + ab;
+            trigger = false // whenever a newline char occurs, set the trigger to false
+        }
+    }
+    
+    return sum
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn one_result() {
-        let query = "duct";
         let contents: String = "eight9fhstbssrplmdlncmmqqnklb39ninejz".to_string();
 
         assert_eq!(18, add_numbers(contents));
@@ -38,8 +78,7 @@ mod tests {
     
     #[test]
     fn error() {
-        let query = "duct";
-        let contents = "eight9fhstbssrplmdlncmmqqnklb39ninejz";
+        let contents: String = "eight9fhstbssrplmdlncmmqqnklb39ninejz".to_string();
 
         assert_eq!(18, add_numbers(contents));
     }
--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,11 +1,13 @@
 use std::fs;
 use day1::add_numbers;
+use day1::add_numbers_2;
 
 fn main() {
 
     let file_path  = "sourcefile.txt";
     let contents = fs::read_to_string(file_path).expect("File not found",);
 
-    println!("{}", add_numbers(contents));
+    println!("{}", add_numbers(contents.clone()));
+    println!("{}", add_numbers_2(contents.clone()));
 }
 
