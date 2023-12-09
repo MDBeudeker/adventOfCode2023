6f930718da89ad2454f2e5f8423e1eb08d4bd003
Maurits Beudeker <mauritsbeu@gmail.com>
2023-12-09 14:37:18 +0100

    day 1-2 with chatGPT

--- day1/src/lib.rs
+++ day1/src/lib.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 pub fn add_numbers(string: String) -> i32 {
     let mut a: i32 = 0; // initialize var a
-    let mut  ab: i32 = 0;
+    let mut ab: i32 = 0;
     let mut sum:i32 = 0;
     let mut trigger: bool = false;
     
@@ -28,37 +28,54 @@ pub fn add_numbers(string: String) -> i32 {
 
 pub fn add_numbers_2(string: String) -> i32 {
     let numbers_dict = HashMap::from([
-        ("zero", 0),
-        ("one", 1),
-        ("two", 2),
-        ("three", 3),
-        ("four", 4),
-        ("five", 5),
-        ("six", 6),
-        ("seven", 7),
-        ("eight", 8),
-        ("nine", 9),
+        ("zero".to_string() , 0),
+        ("one".to_string()  , 1),
+        ("two".to_string()  , 2),
+        ("three".to_string(), 3),
+        ("four".to_string() , 4),
+        ("five".to_string() , 5),
+        ("six".to_string()  , 6),
+        ("seven".to_string(), 7),
+        ("eight".to_string(), 8),
+        ("nine".to_string() , 9),
     ]);
-    let mut a: i32 = 0; // initialize var a
-    let mut  ab: i32 = 0;
-    let mut sum:i32 = 0;
-    let mut trigger: bool = false;
     
-    for c  in string.chars() {
-        if let Some(digit) =  c.to_digit(10) {
-            if !trigger {
-                a = digit as i32;  // if char is the 1st number in the string, save it to 'a'
-                trigger = true;             // 1st character has been found, don't save a on the next char
+    let mut ab: i32 = 0;
+    let mut sum:i32 = 0;
+    
+    for line  in string.lines() {
+    
+        let mut a = 0;
+        let mut b = 0;
+        
+        let mut matches: Vec<(i32, usize)> = Vec::new();
+        for (word, number) in &numbers_dict {
+
+            let mut position = 0;
+            while let Some(found_position) = line[position..].find(word) {
+                position += found_position;
+                matches.push((*number, position));
+                position += word.len();
             }
-            let b: i32 = digit as i32;
-            ab = a *10 + b;
+
+            let number_str = number.to_string();
+            position = 0;
+            while let Some (found_position) = line[position..].find(&number_str) {
+                position += found_position;
+                matches.push((*number, position));
+                position += number_str.len();
+            }
         }
-        //println!("{c}")
-        if (c as i32) == 10 {
-            // println!("{ab}");
-            sum = sum + ab;
-            trigger = false // whenever a newline char occurs, set the trigger to false
+        matches.sort_by(|a, b| a.1.cmp(&b.1));
+        if let Some((first,_)) = matches.first() {
+            a = *first;
         }
+        if let Some((last, _)) = matches.last() {
+            b = *last;
+        }
+        
+        ab = a *10 + b;
+        sum = sum + ab;
     }
     
     return sum
--- day1/src/main.rs
+++ day1/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let file_path  = "sourcefile.txt";
     let contents = fs::read_to_string(file_path).expect("File not found",);
 
-    println!("{}", add_numbers(contents.clone()));
-    println!("{}", add_numbers_2(contents.clone()));
+    println!("Sum of all digits: {}", add_numbers(contents.clone()));
+    println!("Sum of all numbers also as strings: {}", add_numbers_2(contents.clone()));
 }
 
