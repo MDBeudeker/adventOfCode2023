522f029aaca84ddfc7fefd736b2bac2a14234da7
Maurits Beudeker <mauritsbeu@gmail.com>
2023-12-10 12:42:43 +0100

    a whole lot of parsing

--- day2/src/lib.rs
+++ day2/src/lib.rs
@@ -1,4 +1,44 @@
+use std::collections::HashMap;
+
 pub fn count_cubes (string: String) -> i32{
-    string;
-    1
+    let gamecontents: Vec<&str> = string.split(": ").collect();
+    let turns: Vec<&str> =  gamecontents[1].split("; ").collect();
+    let mut counter = 0;
+    let gameid: i32 = gamecontents[0].split_whitespace().nth(1).unwrap().parse().unwrap(); // chatgpt magic
+
+    let mut valid: bool = true;
+
+    println!("game id is {:?}", gameid);
+    for turn in turns {
+        counter = counter+1;
+        valid = verify_turn(turn);
+        println!("turn {}: {}, valid: {}", counter, turn, valid);
+        if !valid {break}
+    }
+    //println!("{}", &gamecontents.next().unwrap());
+    if valid {
+        return gameid
+    } else {return 0}
+}
+
+fn verify_turn(turn:&str) -> bool {
+
+    let cube_possibilities = HashMap::from([
+        ("red".to_string(), 12),
+        ("green".to_string(), 13),
+        ("blue".to_string()  , 14),
+    ]);
+
+    let sets: Vec<&str> = turn.split(", ").collect();
+
+    for set in sets{
+        println!("{}", set);
+        for (color, count) in &cube_possibilities {
+            let colory = set.find(color);
+            println!("{:?}", colory)
+        }
+    }
+
+    true
+
 }
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -4,18 +4,19 @@ use std::collections::HashMap;
 
 fn main() {
 
-    let _cube_possibilities = HashMap::from([
-        ("red".to_string(), 12),
-        ("green".to_string(), 13),
-        ("blue".to_string()  , 14),
-    ]);
+    // let _cube_possibilities = HashMap::from([
+    //     ("red".to_string(), 12),
+    //     ("green".to_string(), 13),
+    //     ("blue".to_string()  , 14),
+    // ]);
 
     let file_path  = "sourcefile.txt";
     let contents = fs::read_to_string(file_path).expect("File not found",);
 
+    let mut sum: i32 = 0;
     if let Some(first_line) = contents.lines().next() {
-        println!("{}", first_line);
-        let a =count_cubes("haha".to_string());
+        //println!("{}", first_line);
+        sum = sum + count_cubes(first_line.to_string());
     } 
-
+    println!("the sum of all valid game numbers is {}", sum)
 }
