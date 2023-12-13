0e7d63ce1730aefbdb3c5740117fecac771a1c3a
MDBeudeker <mauritsbeu@gmail.com>
2023-12-13 21:46:36 +0100

    pooh, that's not honey!!

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -5,17 +5,27 @@ pub fn interpolate(string: String) -> i32 {
         .collect();
 
     println!("original vector: \n{:?}", path_vector);
-    let mut result_vec = Vec::new();
-    vector_difference(&path_vector, &mut result_vec);
+    let mut empty_vec = Vec::new();
+    let mut result_vec = vector_difference(&path_vector, &mut empty_vec);
+    result_vec.insert(0, path_vector.clone());
+    println! {"new vec is {:?}", result_vec}
     1
 }
 
-fn vector_difference(vec: &Vec<i32>, res_vec: &mut Vec<Vec<i32>>) -> Vec<i32> {
-    let mut result_vec: Vec<i32> = Vec::new();
-    for window in vec.windows(2) {
-        let sum = window[1] - window[0];
-        result_vec.push(sum);
+fn vector_difference(vec: &Vec<i32>, res_vec: &mut Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let mut new_vec = Vec::new();
+    if vec.len() > 1 {
+        for window in vec.windows(2) {
+            let sum = window[1] - window[0];
+            new_vec.push(sum);
+        }
+        println!("{:?}", new_vec);
+        res_vec.push(new_vec.clone());
+        if new_vec.iter().all(|&x| x == 0) {
+            return res_vec.clone();
+        }
+        vector_difference(&new_vec, res_vec)
+    } else {
+        res_vec.clone()
     }
-    println!("{:?}", result_vec);
-    result_vec
 }
