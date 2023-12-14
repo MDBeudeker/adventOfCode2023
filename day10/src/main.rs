79d23066d1a4cb0071629452c79fbd4471deb412
MDBeudeker <mauritsbeu@gmail.com>
2023-12-14 17:34:15 +0100

    quick commit

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,15 +1,15 @@
 use std::fs;
 
 fn main() {
-    let file_path  = "sourcefile.txt";
-    let contents = fs::read_to_string(file_path).expect("File not found",);
+    let file_path = "input.txt";
+    let contents = fs::read_to_string(file_path).expect("File not found");
 
     println!("{}", contents);
     let position = contents.chars().position(|c| c == 'S').unwrap();
     println!("position {}", position);
-    
+
     if let Some(first_line) = contents.lines().next() {
         let width = first_line.len();
         println!("width is {}", width)
-    } 
+    }
 }
